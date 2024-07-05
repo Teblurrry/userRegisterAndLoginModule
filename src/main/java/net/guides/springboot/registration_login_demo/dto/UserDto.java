ef5d4a9d8d5d48e951f4d9ab874634ff10707be4
cynthiame <cynthia620ryn@outlook.com>
2024-07-05 16:29:54 +0800

    Initial commit

---
+++ src/main/java/net/guides/springboot/registration_login_demo/dto/UserDto.java
@@ -0,0 +1,30 @@
+package net.guides.springboot.registration_login_demo.dto;
+//transfer data between controller layer and the view layer && form binding
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty(message="Email should not be empty")
+    @Email
+    private String email;
+
+    @NotEmpty(message="Password should not be empty")
+    private String password;
+}
